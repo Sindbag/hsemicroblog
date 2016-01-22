200b51f2054e04326c0b828aa7f3ebe0a5652a1b
Anatoly <sindbag@gmail.com>
2016-01-23 00:06:48 +0300

    Update settings.py

--- microblog/settings.py
+++ microblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&9d_8ehqk_c!s)7s0y(7=iqn2t=r!g85z54#a^1p9eg&@bbl9r'
+SECRET_KEY = 'KEKOS PASHALKA'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
